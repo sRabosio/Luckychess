5eccd348b88375547cd79f2750146ab49f0cac93
sRabosio <rabosiosimone@gmail.com>
2023-11-26 17:41:40 +0100

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports ={
+    mode: process.env.NODE_ENV ? 'jit' : undefined,
+    content: [
+        './src/main/resources/*.html',
+        './src/main/resources/templates/*.html'
+        ],
+    darkMode: 'media',
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: []
+}
