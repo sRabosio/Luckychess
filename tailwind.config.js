c5ecc0dafbb7691e505cc285fcad9f63ea020e40
sRabosio <rabosiosimone@gmail.com>
2023-12-11 18:18:29 +0100

    luckychessLight theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,26 @@
-module.exports ={
+module.exports = {
     mode: process.env.NODE_ENV ? 'jit' : undefined,
     content: [
         './src/main/resources/*.html',
         './src/main/resources/templates/**/*.html'
         ],
     darkMode: 'media',
-    theme: {
-        extend: {
-        },
+    daisyui:{
+        themes: [
+                {
+                    'luckychessDark':{
+                        ...require('daisyui/src/theming/themes')['dark'],
+                        'primary': '#ceaf8a',
+                        'secondary': '#3d4451',
+                        'accent': '#37cdbe',
+                        'neutral': '#d9d9d9',
+                        'base-100': '#fDfDfD',
+                    }
+                }
+            ],
     },
     variants: {
         extend: {},
     },
-    plugins: [require("daisyui")]
+    plugins: [require('daisyui')]
 }
